c80c4e965e02b7d4a1025adf3ce644e02f4c347a
Collins-01 <oriakhicolls01@gmail.com>
2025-10-21 16:33:57 +0100

    adde health controller

---
+++ src/main/java/com/collins/ploutos/ploutos/PloutosApplication.java
@@ -0,0 +1,13 @@
+package com.collins.ploutos.ploutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collins/ploutos/ploutos/PloutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collins.ploutos.ploutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
